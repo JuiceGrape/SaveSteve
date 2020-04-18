e1b9a7bfb5826649967b2acc363a44509500a547
Niels Verheijen <kahlan147@gmail.com>
2020-04-18 15:41:07 +0200

    Crusher is armable

--- Assets/Scripts/Traps/Crusher.cs
+++ Assets/Scripts/Traps/Crusher.cs
@@ -5,9 +5,55 @@ using UnityEngine;
 public class Crusher : Trap
 {
 
-    public override void Trigger()
+    private bool isArmed;
+    private bool isActivated;
+
+    [SerializeField]
+    private TrapButton myButton = null;
+    [SerializeField]
+    protected Animator myAnimator;
+
+    protected Collider2D myCollider;
+    [SerializeField]
+    protected Collider2D deathzone;
+
+    public void Start()
+    {
+        myCollider = GetComponent<Collider2D>();
+        myCollider.isTrigger = true;
+        myCollider.enabled = false;
+    }
+
+    public void Trigger()
     {
         myAnimator.SetTrigger("TrapTriggered");
+    }
+
+    public override void ArmTrap()
+    {
+        isArmed = true;
+        myCollider.enabled = true;
+    }
+
+    public void ActivateDeathzone()
+    {
+        deathzone.enabled = true;
+    }
+
+    public void finishedAttacking()
+    {
+        isArmed = false;
+        myButton.activateCooldown();
+        myCollider.enabled = false;
+        deathzone.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Adventurer>() && isArmed)
+        {
+            Trigger();
+        }
     }
 
 }
