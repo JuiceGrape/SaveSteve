7775879ae56a8d84b362b05cf2f4dd1c66f99303
Bas Mentjot <bmentjot@gmail.com>
2020-04-18 15:41:24 +0200

    steve chompies

---
+++ Assets/Scripts/ChompOccasionally.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChompOccasionally : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+       // StartCoroutine(chompAfterAndRepeat(5.0f));
+    }
+
+    private void OnMouseDown() 
+    {
+        animator.SetTrigger("StartEat");
+    }
+
+    IEnumerator chompAfterAndRepeat(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        animator.SetTrigger("StartEat");
+        StartCoroutine(chompAfterAndRepeat(seconds));
+    }
+
+}
