9d383732471315410c0758a285f8ba80ee8aa32e
Bas Mentjot <bmentjot@gmail.com>
2020-04-18 12:06:56 +0200

    Added camera stuff and enemy stuff

--- Assets/Scripts/Adventurer/Adventurer.cs
+++ Assets/Scripts/Adventurer/Adventurer.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Adventurer : MonoBehaviour
 {
     [SerializeField]
     private List<TrapType> immuneToTraps;
+
+    public float speed = 1;
 
 
     void Start()
@@ -16,6 +17,11 @@ public class Adventurer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate() 
+    {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
     }
 
     public void AttackedByTrap(TrapType trapType)
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float cameraMovementSpeed = 1.0f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector2(-cameraMovementSpeed * Time.deltaTime, 0));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2(cameraMovementSpeed * Time.deltaTime, 0));
+        }
+    }
+}
