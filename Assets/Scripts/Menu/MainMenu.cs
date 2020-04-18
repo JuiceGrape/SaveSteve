3c054a26ab624f884c9b3253da13ec53d17a67d0
Niels Verheijen <kahlan147@gmail.com>
2020-04-18 12:57:23 +0200

    Main menu & Level select

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject LevelSelect;
+    [SerializeField] private GameObject MainMenuOptions;
+
+    private void Start()
+    {
+        ShowMainMenu();
+    }
+
+    public void StartGame()
+    {
+        LoadLevel(1);
+    }
+
+    public void LoadLevel(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+
+    public void EndlessMode()
+    {
+
+    }
+
+    public void ShowLevelSelect()
+    {
+        LevelSelect.SetActive(true);
+        MainMenuOptions.SetActive(false);
+    }
+
+    public void ShowMainMenu()
+    {
+        LevelSelect.SetActive(false);
+        MainMenuOptions.SetActive(true);
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
