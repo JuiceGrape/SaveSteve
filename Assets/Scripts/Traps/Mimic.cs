97e110a44906a6bd9f4942b757eb1910001c071f
Niels Verheijen <kahlan147@gmail.com>
2020-04-19 19:31:55 +0200

    Mimic

---
+++ Assets/Scripts/Traps/Mimic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mimic : Trap
+{
+
+    public override void Trigger(GameObject collisionObject)
+    {
+        base.Trigger(null);
+        collisionObject.GetComponent<Adventurer>().Die();
+    }
+
+    public override void ArmTrap()
+    {
+        myAnimator.SetTrigger("TrapArmed");
+        base.ArmTrap();
+    }
+}
