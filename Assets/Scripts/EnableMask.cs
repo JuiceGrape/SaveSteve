06913561c8918fff8f7160171c3b829eba8cd32d
Shi <g.vandenbiggelaar@gmail.com>
2020-04-19 13:42:52 +0200

    main menu chomp

---
+++ Assets/Scripts/EnableMask.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableMask : MonoBehaviour
+{
+    public SpriteMask mask;
+
+    public void Enablexd() {
+        mask.enabled = true;
+    }
+}
--- Assets/Scripts/Steve/ChompOccasionally.cs
+++ Assets/Scripts/Steve/ChompOccasionally.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ChompOccasionally : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] private string animation;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class ChompOccasionally : MonoBehaviour
 
     private void OnMouseDown() 
     {
-        animator.SetTrigger("StartEat");
+        animator.SetTrigger(animation);
     }
 
     IEnumerator chompAfterAndRepeat(float seconds)
